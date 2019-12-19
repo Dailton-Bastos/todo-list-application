9eae99a4ecc725b3a77aa68e2f6c0c5da12fe823
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-19 15:11:28 -0300

    Respostas JSON

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
   res.send("<h1>Minha lista de tarefas</h1>");
 });
 
+app.get("/json", (req, res) =>
+  res.json({
+    title: "Tarefa X",
+    done: true
+  })
+);
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}`);
 });
