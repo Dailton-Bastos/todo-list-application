69d1ccc23652fb276eb8e794e9aa4c27d96060c3
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-19 15:05:12 -0300

    Primeira roda com Express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
