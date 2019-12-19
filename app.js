022ec0681d0b2e77545091293357c688f5886234
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-19 16:06:31 -0300

    Concentrando rotas

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const checkListRouter = require("./src/routes/checklist");
 
 // Middlewares
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data atual: ${Date.now()}`);
-  next();
-};
-
-app.use(log);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Minha lista de tarefas</h1>");
-});
-
-app.get("/json", (req, res) => {
-  res.json({
-    title: "Tarefa X",
-    done: true
-  });
-});
+app.use("/checklists", checkListRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}`);
