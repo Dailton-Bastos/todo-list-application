1987f6ed8e20cdf8fbf55214712e2336a0859233
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-19 22:39:45 -0300

    Instalação do Mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const checkListRouter = require("./src/routes/checklist");
+require("./config/database");
 
 // Middlewares
 app.use(express.json());
