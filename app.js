d8456f9bc35bddba446c4dcf24c129af66fac9a5
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-20 11:00:42 -0300

    EJS e configuração com o Express

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 3000;
 const checkListRouter = require("./src/routes/checklist");
+const rooterRouter = require("./src/routes/index");
 require("./config/database");
 
 // Middlewares
 app.use(express.json());
 
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
+app.use("/", rooterRouter);
 app.use("/checklists", checkListRouter);
 
 app.listen(PORT, () => {
